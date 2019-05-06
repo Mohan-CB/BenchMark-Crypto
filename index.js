e4a203b8f14a24fbc564a9dedb956181a250a70c
Mohan Liu <Mohan.Liu@careerbuilder.com>
2019-05-06 15:38:48 +0800

    init commit

---
+++ index.js
@@ -0,0 +1,25 @@
+var crypto_js256 = require('crypto-js/sha256');
+var ncrypto = require('crypto');
+var faker = require('faker');
+
+size = process.argv[2] || 100;
+console.log(`size ${size}`);
+var sampleData = [];
+for(i = 0; i < size; i++){
+  sampleData.push(faker.internet.email());
+}
+
+start = Date.now();
+for( email of sampleData){
+ crypto_js256(email).toString();
+}
+end = Date.now() - start;
+console.log(`[crypto-js] : ${end}`);
+
+start = Date.now();
+for( email of sampleData){
+  ncrypto.createHash('sha256').update(email).digest('hex');
+
+}
+end = Date.now() - start;
+console.log(`[native-crypto] : ${end}`);
---
+++ readme.md
@@ -0,0 +1,17 @@
+benchmark test for crypto-js and native crypto module
+
+## How to RUN
+node index.js [10|100|1000|1000]
+
+# Report
+size 10
+[crypto-js] : 1
+[native-crypto] : 1
+
+size 100
+[crypto-js] : 9
+[native-crypto] : 1
+
+size 1000
+[crypto-js] : 26
+[native-crypto] : 3
