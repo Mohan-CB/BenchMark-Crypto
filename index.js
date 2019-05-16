fb566457be285e7dd2c1f076c4956c43fce2f894
Mohan Liu <Mohan.Liu@careerbuilder.com>
2019-05-16 22:10:59 +0800

    256, 512, 1024 all works

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
-var crypto_js256 = require('crypto-js/sha256');
-var ncrypto = require('crypto');
-var faker = require('faker');
+var CryptoJS = require('crypto-js');
 
-size = process.argv[2] || 100;
-console.log(`size ${size}`);
-var sampleData = [];
-for(i = 0; i < size; i++){
-  sampleData.push(faker.internet.email());
-}
+var key512key = 'B?E(H+MbQeThVmYq3t6w9z$C&F)J@NcRfUjXnZr4u7x!A%D*G-KaPdSgVkYp3s5v';
+var key256key = 'J@NcRfUjWnZr4u7x!A%D*G-KaPdSgVkY';
+var key1024key='cRfUjXn2r5u8x!A%D*G-KaPdSgVkYp3s6v9y$B?E(H+MbQeThWmZq4t7w!z%C*F)J@NcRfUjXn2r5u8x/A?D(G+KaPdSgVkYp3s6v9y$B&E)H@McQeThWmZq4t7w!z%C';
 
-start = Date.now();
-for( email of sampleData){
- crypto_js256(email).toString();
-}
-end = Date.now() - start;
-console.log(`[crypto-js] : ${end}`);
 
-start = Date.now();
-for( email of sampleData){
-  ncrypto.createHash('sha256').update(email).digest('hex');
+var message = 'hello';
 
-}
-end = Date.now() - start;
-console.log(`[native-crypto] : ${end}`);
+var encrypted256 = CryptoJS.AES.encrypt(message, key256key);
+console.log(encrypted256.toString());
+var encrypted512 = CryptoJS.AES.encrypt(message, key512key);
+console.log(encrypted512.toString());
+var encrypted1024 = CryptoJS.AES.encrypt(message, key1024key);
+console.log(encrypted1024.toString());
+
+
+var decrypted256 = CryptoJS.AES.decrypt(encrypted256, key256key);
+console.log(decrypted256.toString(CryptoJS.enc.Utf8));
+var decrypted512 = CryptoJS.AES.decrypt(encrypted512, key512key);
+console.log(decrypted512.toString(CryptoJS.enc.Utf8));
+var decrypted1024 = CryptoJS.AES.decrypt(encrypted1024, key1024key);
+console.log(decrypted1024.toString(CryptoJS.enc.Utf8));
